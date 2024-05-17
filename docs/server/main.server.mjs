1d0f5c7dd7be95261ba0a4f0e9f9cce4d1dc81e9
AliYusubov <120775751+AliYusubov@users.noreply.github.com>
2024-05-17 16:49:20 +0400

    fix

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-223W3AS2.mjs";import"./chunk-UPBJXYP7.mjs";import"./chunk-47S6SCV7.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-WIMCL2Z5.mjs";import"./chunk-RKIMS5LY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
